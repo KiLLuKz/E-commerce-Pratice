f5207f23a6aded1fe8a813ab20207a3dc012aa4e
KiLLuKz <quemaster123@gmail.com>
2026-05-02 17:47:52 +0700

    Setup Files

--- src/main.js
+++ src/main.js
@@ -1,6 +1,1 @@
 import './style.css'
-
-const button = document.querySelector('#btn');
-button.addEventListener('click', () => {
-  document.querySelector('#title').innerText = 'ยินดีด้วย! JS ทำงานแล้ว';
-});
